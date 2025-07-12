632640cbc6fee5dc56476c769f41a493b08ebee5
“iamwm999” <iamwm920@aol.com>
2025-07-12 12:46:14 -0700

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scrolling for anchor links
+const links = document.querySelectorAll('a[href^="#"]');
+links.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
